73ba76cdaf20babe7c814c0090deaf0b24cdd593
idumploader <pedropedro123112@gmail.com>
2026-05-31 07:58:43 +0300

    Small refactor

--- src/ModLoader/Hook.hpp
+++ src/ModLoader/Hook.hpp
@@ -42,7 +42,7 @@ struct ThisBoundMethod {
  * @return Pointer to child's VFTable
  */
 template<typename T, typename TVFTable = typename T::VFTable>
-[[nodiscard]] constexpr TVFTable* cast_vftable(T* object) {
+[[nodiscard]] constexpr TVFTable* cast_vftable(T* object) noexcept {
     return static_cast<TVFTable*>(object->vftable);
 }
 
@@ -55,7 +55,7 @@ template<typename T, typename TVFTable = typename T::VFTable>
  * @return VFTable bound method. Can be used to call actual method
  */
 template<typename T, typename TMethod>
-[[nodiscard]] constexpr auto bind_vftable(T* object, TMethod method) {
+[[nodiscard]] constexpr auto bind_vftable(T* object, TMethod method) noexcept {
     return ThisBoundMethod{ object, cast_vftable(object)->*method };
 }
 
@@ -75,7 +75,7 @@ template<typename T, typename TMethod>
  * @return Offsetted address pointer
  */
 template<typename T>
-[[nodiscard]] inline T at_offset(void* base, intptr_t offset) {
+[[nodiscard]] inline T at_offset(void* base, intptr_t offset) noexcept {
     return std::bit_cast<T>(reinterpret_cast<char*>(base) + offset);
 }
 
@@ -87,7 +87,7 @@ template<typename T>
  * @return Value at offsetted address
  */
 template<typename T>
-[[nodiscard]] inline T& at_address(void* base, intptr_t offset) {
+[[nodiscard]] inline T& at_address(void* base, intptr_t offset) noexcept {
     return *at_offset<std::add_pointer_t<T>>(base, offset);
 }
 
@@ -114,13 +114,38 @@ struct RTTIObjectLocator {
     const void* hierarchy_reference;
 };
 
+static_assert(offsetof(RTTIObjectLocator, signature) == 0x0);
+static_assert(offsetof(RTTIObjectLocator, vftable_offset) == 0x4);
+static_assert(offsetof(RTTIObjectLocator, ctx_displacement_offset) == 0x8);
 static_assert(offsetof(RTTIObjectLocator, type_info) == 0xC);
+static_assert(offsetof(RTTIObjectLocator, hierarchy_reference) == 0x10);
+static_assert(sizeof(RTTIObjectLocator) == 0x14);
 
 /**
  * Get @ref RTTIObjectLocator from pointer to virtual class containing it's information. *Unsafe*
  * @param object Pointer to virtual class
  * @return The class object locator
  */
-inline RTTIObjectLocator* get_object_locator(void* object) {
-    return at_address<RTTIObjectLocator*>(at_address<void*>(object, 0), -intptr_t(sizeof(void*)));
+[[nodiscard]] inline RTTIObjectLocator* get_object_locator(void* object) noexcept {
+    static constexpr intptr_t rtti_offset = -static_cast<intptr_t>(sizeof(void*));
+    void* vftable = at_address<void*>(object, 0);
+    return at_address<RTTIObjectLocator*>(vftable, rtti_offset);
+}
+
+/**
+ * Check if a given virtual object is a given class
+ * @return true if object is that class, false otherwise
+ */
+template<typename T>
+    requires requires { T::type_name; }
+[[nodiscard]] inline bool is_vlocation(const RTTIObjectLocator* loc) noexcept {
+    if (!loc || !loc->type_info) return false;
+    return loc->type_info->name() == T::type_name;
+}
+
+template<typename T>
+    requires requires { T::type_name; }
+[[nodiscard]] inline bool is_vclass(const void* object) noexcept {
+    if (!object) return false;
+    return is_vlocation<T>(get_object_locator(const_cast<void*>(object)));
 }
